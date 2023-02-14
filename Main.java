748dff34213af06fa346e6d42d5d5f054252e57b
IrmakAkatay <123840414+IrmakAkatay@users.noreply.github.com>
2023-02-14 18:56:22 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int km ;
+        double  perKm =2.20,total=10;
+
+        Scanner wab =new Scanner(System.in);
+        System.out.print("Kilometre Değerini Giriniz :");
+        km= wab.nextInt();
+
+        total +=(perKm*km) ;
+        total = (total < 20) ? 20 : total ;
+        System.out.println("Tutar : "+total);
+
+
+    }
+    }
